db1bb549d65bc35fde16223918c044cca04cfa9b
DESKTOP-T9I8S8D\root <a>
2023-07-25 23:52:41 -0300

    Test Unitarios y de Integración

--- src/models/libroModel.js
+++ src/models/libroModel.js
@@ -1,15 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_DB, {
+mongoose.connect("mongodb://127.0.0.1/biblioteca", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
 });
 
-const LibroSchema = new mongoose.Schema({
-  titulo: String,
-  autor: String
-}, { collection: 'libros' });
+const LibroSchema = new mongoose.Schema(
+  {
+    titulo: String,
+    autor: String,
+  },
+  { collection: "libros" }
+);
 
-const Libro = mongoose.model('Libro', LibroSchema);
+const Libro = mongoose.model("Libro", LibroSchema);
 
 module.exports = Libro;
